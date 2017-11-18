1a52c63863a675a01ad17ff0ed5a4150ee2653bf
Margaryta Tchouikevitch <margaryta@gmail.com>
2017-11-18 18:34:44 -0500

    several bug fixes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:dashboard, :index, :show]
+  before_action :check_correct_user, only: [:edit, :update, :destroy]
   # GET /tweets
   # GET /tweets.json
   def index
@@ -64,6 +65,7 @@ class TweetsController < ApplicationController
   def dashboard
     if current_user
       @tweets = current_user.tweets
+      @user = current_user
     else
       @tweets = Tweet.order('created_at DESC').limit(25)
     end
@@ -80,4 +82,10 @@ class TweetsController < ApplicationController
     def tweet_params
       params.require(:tweet).permit(:content)
     end
+
+  def check_correct_user
+    unless current_user && @tweet.user == current_user
+      redirect_to tweets_url, notice: "You cannot do that"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
 
   def follow
     @user = User.find(params[:id])
-    current_user.following_users << @user
-    redirect_to show_user_url(@user)
+
+    if current_user.following_users.include?(@user)
+      current_user.following_users.destroy(@user)
+      redirect_to tweets_path
+    else
+      current_user.following_users << @user
+      redirect_to show_user_url(@user)
+    end
   end
 end
